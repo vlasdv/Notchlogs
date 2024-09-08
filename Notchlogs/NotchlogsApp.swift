458eba3904a8cd9b3cf55ec68dcfe402a45944f5
Dmitrii Vlasov <vlasdev@icloud.com>
2024-09-08 22:09:00 +0200

    Add project files

---
+++ Notchlogs/NotchlogsApp.swift
@@ -0,0 +1,17 @@
+//
+//  NotchlogsApp.swift
+//  Notchlogs
+//
+//  Created by Dmitrii Vlasov on 08/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct NotchlogsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
