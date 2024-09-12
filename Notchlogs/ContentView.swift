4c7e73bd1c355875452cf250d376c804ebfdb000
Dmitrii Vlasov <vlasdev@icloud.com>
2024-09-12 22:24:04 +0200

    Add message grouping into sections

--- Notchlogs/ContentView.swift
+++ Notchlogs/ContentView.swift
@@ -35,6 +35,9 @@ struct Message: Identifiable, Hashable {
 }
 
 struct ContentView: View {
+    @State var lastMessage: Message?
+    @State var combinedMessageText = ""
+
     @State var messages = [
         Message(date: Date.now, text: "Some message to 1"),
         Message(date: Date.now, text: "Some message to 2"),
@@ -56,14 +59,31 @@ struct ContentView: View {
         Message(date: Date.now, text: "Some message to 9"),
         Message(date: Date.now, text: "Some message to 1"),
         Message(date: Date.now, text: "Some message to 2"),
-        Message(date: Date.now, text: "Some message to 3"),
         Message(date: Date.now, text: "Some message to 4"),
-        Message(date: Date.now, text: "Some message to 5"),
-        Message(date: Date.now, text: "Some message to 6"),
-        Message(date: Date.now, text: "Some message to 7"),
-        Message(date: Date.now, text: "Some message to 8"),
-        Message(date: Date.now, text: "Some message to 9Some message to 9Some message to 9Some message to 9Some message to 9")
+        Message(date: Date.now, text: "Some message to 4"),
+        Message(date: Date.now, text: "Some message to 4"),
+        Message(date: Date.now.addingTimeInterval(50), text: "Some message to 7"),
+        Message(date: Date.now.addingTimeInterval(100), text: "Some message to 7"),
+        Message(date: Date.now.addingTimeInterval(100), text: "Some message to 7"),
+        Message(date: Date.now.addingTimeInterval(200), text: "Some message to 9Some message to 9Some message to 9Some message to 9Some message to 9")
     ]
+
+    private var groupedMessages: [[Message]] {
+        var group = [Message]()
+        var result = [[Message]]()
+        var lastMessage: Message?
+
+        for message in messages {
+            if (message.text == lastMessage?.text) || group.isEmpty {
+                group.append(message)
+            } else {
+                result.append(group)
+                group = [message]
+            }
+            lastMessage = message
+        }
+        return result
+    }
 
     @State var inputText = ""
 
@@ -72,20 +92,25 @@ struct ContentView: View {
             VStack {
                 ScrollViewReader { proxy in
                     List {
-                        ForEach(messages) { message in
-                            HStack(alignment: .top) {
-                                Text(message.text)
-                                    .id(message.id)
-                                Spacer(minLength: 10)
-                                Text(message.date.formatted(date: .omitted, time: .shortened))
-                                    .font(.footnote)
-                                    .foregroundStyle(.secondary)
+                        ForEach(groupedMessages, id: \.self) { messages in
+                            Section {
+                                ForEach(messages) { message in
+                                    HStack(alignment: .top) {
+                                        Text(message.text)
+                                            .id(message.id)
+                                        Spacer(minLength: 10)
+                                        Text(message.date.formatted(date: .omitted, time: .shortened))
+                                            .font(.footnote)
+                                            .foregroundStyle(.secondary)
+                                    }
+                                }
+                                .onDelete(perform: { indexSet in
+                                    //                                messages.remove(atOffsets: indexSet)
+                                })
                             }
                         }
-                        .onDelete(perform: { indexSet in
-                            messages.remove(atOffsets: indexSet)
-                        })
                     }
+                    .listSectionSpacing(10.0)
                     .onAppear {
                         if let lastItem = messages.last {
                             print("last found")
